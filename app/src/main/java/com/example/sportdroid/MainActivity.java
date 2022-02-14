4baedc89e1186cb502e12bbc5a8e7d07f5485936
simon <simon.p5207@gmail.com>
2022-02-14 13:46:57 +0100

    ajout activité

--- app/src/main/java/com/example/sportdroid/MainActivity.java
+++ app/src/main/java/com/example/sportdroid/MainActivity.java
@@ -9,24 +9,39 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button button;
+    private Button ButtonCalendrier;
+    private Button ButtonEntrainement;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
-        button = (Button) findViewById(R.id.button2);
-        button.setOnClickListener(new View.OnClickListener() {
+        ButtonCalendrier = (Button) findViewById(R.id.ButtonCalendrier);
+        ButtonCalendrier.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 calender();
             }
         });
+
+        ButtonEntrainement= (Button) findViewById(R.id.ButtonEntrainement);
+        ButtonEntrainement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                entrainement();
+            }
+        });
+
     }
 
     public void calender(){
         Intent intent = new Intent(this, Calendar.class);
+        startActivity(intent);
+    }
+    public void entrainement(){
+        Intent intent = new Intent(this, entrainement.class);
         startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/sportdroid/ajout_entrainement.java
@@ -0,0 +1,14 @@
+package com.example.sportdroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ajout_entrainement extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ajout_entrainement);
+    }
+}
